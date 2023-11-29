e86be5a6d536554f9d29172f83efa22009b73fa1
Bob Bobety <andrej.ljubic05@hotmail.com>
2023-11-29 20:10:14 +0000

    Substitution 1 and 2, Rail-Fence

---
+++ cryptography/substitution0/solve.py
@@ -0,0 +1,30 @@
+from string import ascii_uppercase, ascii_lowercase
+
+alphabet = 'ZGSOCXPQUYHMILERVTBWNAFJDK'
+
+text = '''
+Qctcnrel Mcptzlo ztebc, fuwq z ptzac zlo bwzwcmd zut, zlo gtenpqw ic wqc gccwmc
+xtei z pmzbb szbc ul fqusq uw fzb clsmebco. Uw fzb z gcznwuxnm bsztzgzcnb, zlo, zw
+wqzw wuic, nlhlefl we lzwntzmubwb—ex sentbc z ptczw rtukc ul z bsuclwuxus reulw
+ex aucf. Wqctc fctc wfe tenlo gmzsh brewb lczt elc cjwtciuwd ex wqc gzsh, zlo z
+melp elc lczt wqc ewqct. Wqc bszmcb fctc cjsccoulpmd qzto zlo pmebbd, fuwq zmm wqc
+zrrcztzlsc ex gntlubqco pemo. Wqc fcupqw ex wqc ulbcsw fzb actd tcizthzgmc, zlo,
+wzhulp zmm wqulpb ulwe selbuoctzwuel, U senmo qztomd gmzic Ynruwct xet qub eruluel
+tcbrcswulp uw.
+
+Wqc xmzp ub: ruseSWX{5NG5717N710L_3A0MN710L_357GX9XX}
+'''
+
+dec = ''
+
+for c in text:
+    if c in ascii_uppercase:
+        dec += ascii_uppercase[alphabet.index(c)]
+    elif c in ascii_lowercase:
+        dec += ascii_lowercase[alphabet.index(c.upper())]
+    else:
+        dec += c
+
+print(dec)
+
+# picoCTF{5UB5717U710N_3V0LU710N_357BF9FF}
